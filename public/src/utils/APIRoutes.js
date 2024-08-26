e1aac35ef09e5dc6b1b8583c700165448af4db2b
akencoded <114900191+akencoded@users.noreply.github.com>
2024-08-26 18:33:07 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://real-time-chat-repo.onrender.com";
+export const host = "https://real-time-chat-repo.vercel.app/";
 export const registerRoute  = `${host}/api/auth/register`;
 export const loginRoute  = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
